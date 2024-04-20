bf1d8f6da5e2b6b3c7390d0b81a62d575f28678f
Nikolay Ursachi <kolea2002ursaki@gmail.com>
2024-04-20 11:58:39 +0300

    add Task2

---
+++ Desktop/C# projekt/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i1 = 5;
+        int i2 = 6;
+        int sum = i1 + i2;
+        Console.WriteLine(sum);
+
+        long l1 = 6;
+        long sum2 = l1 + i1;
+        Console.WriteLine(sum2);
+
+        double d1 = 5.5;
+        double sum3 = d1 + i1;
+        Console.WriteLine(sum3);
+
+        int product1 = i1 x i2;
+        long produkt2 = l1 x i1;
+        double produkt3 = i1 x d1;
+        Console.WriteLine(product1);
+        Console.WriteLine(product2);
+        Console.WriteLine(product3);
+
+        int quolient = i1 / i2;
+        Console.WriteLine(quotient);
+
+        d1 = 6;
+        double quotient2 = l1 / d1
+        Console.WriteLine(quotient2);
+
+
+
+        
+        
+    }
+}
